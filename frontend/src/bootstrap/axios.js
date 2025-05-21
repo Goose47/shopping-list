65979f7fb2ad12ee7bb7b5bdeee791b90c0888be
Nikita <tyumin000@gmail.com>
2025-05-21 21:39:53 +0300

    add axios base url

--- frontend/src/bootstrap/axios.js
+++ frontend/src/bootstrap/axios.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 const setupAxios = () => {
     window.axios = axios;
     window.axios.defaults.headers.common['Telegram-Init'] = window.Telegram.WebApp.initData;
+    axios.defaults.baseURL = 'https://api.rwfshr.ru';
 }
 
 export {setupAxios}
