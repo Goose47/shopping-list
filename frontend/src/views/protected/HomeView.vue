0853d5f067001d83b5d1816cced312167e999829
Nikita <tyumin000@gmail.com>
2025-05-26 19:29:53 +0300

    test telegram lib

--- frontend/src/views/protected/HomeView.vue
+++ frontend/src/views/protected/HomeView.vue
@@ -1,26 +1,57 @@
 <template>
- <div>Hello from protected!</div>
+  <div
+      v-for="color in colors"
+      :style="{
+        'background-color': wa.themeParams['color'],
+        'color': wa.themeParams['color'],
+        'width': '100px',
+        'height': '40px',
+        'margin-bottom': '10px',
+      }"
+  >
+    {{color}}
+  </div>
+  <div>{{ tg }}</div>
 </template>
 
 <script setup>
-const tg = window.Telegram.WebApp
-tg.MainButton.setText("Подтвердить");
-tg.MainButton.show();
-tg.MainButton.onClick(() => {
-  tg.sendData(JSON.stringify({ action: "confirm" }));
+const tg = window.Telegram
+const wa = tg.WebApp
+wa.MainButton.setText("Подтвердить");
+wa.MainButton.show();
+wa.MainButton.onClick(() => {
+  wa.sendData(JSON.stringify({ action: "confirm" }));
 });
 
 // Secondary Button 1
-const cancelBtn = tg.createSecondaryButton();
+const cancelBtn = wa.createSecondaryButton();
 cancelBtn.setText("Отмена");
-cancelBtn.onClick(() => tg.close());
+cancelBtn.onClick(() => wa.close());
 
 // Secondary Button 2
-const infoBtn = tg.createSecondaryButton();
+const infoBtn = wa.createSecondaryButton();
 infoBtn.setText("Подробнее");
 infoBtn.onClick(() => {
   alert("Здесь можно показать дополнительную информацию.");
 });
+
+const colors = [
+  'accent_text_color',
+  'bg_color',
+  'bottom_bar_bg_color',
+  'button_color',
+  'button_text_color',
+  'destructive_text_color',
+  'header_bg_color',
+  'hint_color',
+  'link_color',
+  'secondary_bg_color',
+  'section_bg_color',
+  'section_header_text_color',
+  'section_separator_color',
+  'subtitle_text_color',
+  'text_color',
+]
 </script>
 
 <style scoped lang="scss">
