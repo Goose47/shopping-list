e046adad017cad9c29b348207a6a06701aaf10f9
Nikita <tyumin000@gmail.com>
2025-05-26 19:21:49 +0300

    test telegram lib

--- frontend/src/views/protected/HomeView.vue
+++ frontend/src/views/protected/HomeView.vue
@@ -3,7 +3,24 @@
 </template>
 
 <script setup>
+const tg = window.Telegram.WebApp
+tg.MainButton.setText("Подтвердить");
+tg.MainButton.show();
+tg.MainButton.onClick(() => {
+  tg.sendData(JSON.stringify({ action: "confirm" }));
+});
 
+// Secondary Button 1
+const cancelBtn = tg.createSecondaryButton();
+cancelBtn.setText("Отмена");
+cancelBtn.onClick(() => tg.close());
+
+// Secondary Button 2
+const infoBtn = tg.createSecondaryButton();
+infoBtn.setText("Подробнее");
+infoBtn.onClick(() => {
+  alert("Здесь можно показать дополнительную информацию.");
+});
 </script>
 
 <style scoped lang="scss">
